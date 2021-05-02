6b1179e75ce45014bf832339f8c4a4618f1df54a
Robert Tucker <r.tucker@quadient.com>
2021-05-02 01:27:38 -0400

    Initial commit to source control

---
+++ src/main/java/com/quadient/spt/demo/Scaler2KafkaDemoApplication.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2021 Quadient Group AG
+ */
+package com.quadient.spt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scaler2KafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scaler2KafkaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quadient/spt/demo/Scaler2KafkaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2021 Quadient Group AG
+ */
+package com.quadient.spt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scaler2KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
